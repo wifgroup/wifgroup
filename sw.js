e9a4115bd3221a2919302bb0312fbaebca1e5ffe
Dikshant Gajera <dikshantgajera@gmail.com>
2026-05-04 11:54:44 +0530

    Fix: clear SW cache, remove old Bootstrap pages and 700KB of unused CSS/JS

--- sw.js
+++ sw.js
@@ -1,25 +1,10 @@
-const cacheName = 'wifgroup-cache-v1';
-const assetsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/assets/img/favicon/favicon.ico'
-];
-
-// Install Service Worker
-self.addEventListener('install', event => {
+// Service worker: clear all caches and unregister self
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(assetsToCache);
-    })
-  );
-});
-
-// Fetch Cached Files
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
+      .then(() => self.registration.unregister())
   );
 });
